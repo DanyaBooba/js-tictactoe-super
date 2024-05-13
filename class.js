d9b95c8d06604b9441d06ac82db148f15f9af507
Daniil Dybka <danil.dybko@gmail.com>
2024-05-13 22:04:54 +0300

    Работает с классом

--- class.js
+++ class.js
@@ -1,9 +1,15 @@
 class TicTacToe {
-	constructor(size = 3) {
-		this.size = size;
+    constructor(size = 3) {
+        this.size = size;
+
+        for (let x = 0; x < size; x++) {
+            for (let y = 0; y < size; y++) {
+                this.array[x][y] = -1;
+            }
+        }
 	}
 
-	get win() {
-		return false;
-	}
+    get win() {
+        return false;
+    }
 }
--- logic.js
+++ logic.js
@@ -1,54 +1,48 @@
 let fieldName = "field";
 let sizeOfField = 3;
 
-function codeBlock(number, size) {
-	let id = `${fieldName}-${number}`;
-	// return `<div id='${id}' style='width: ${size}%'><button onClick='setItem("${id}")'></button></div>`;
-	return `<div id='${id}'><button onClick='setItem("${id}")'></button></div>`;
+function codeBlock(number) {
+    let id = `${fieldName}-${number}`;
+    return `<div id='${id}'><button onClick='setItem("${id}")'></button></div>`;
 }
 
 function setItem(id) {
-	let button = document.getElementById(id);
-	if (!button) return;
-	alert(button);
+    let button = document.getElementById(id);
+    if (!button) return;
+    alert(button);
 }
 
-function createField(cssSizeOfBlock) {
-	for (let x = 0; x < sizeOfField; x++) {
-		let local_div = document.createElement("div");
-		local_div.classList.add("field__item");
+function createField() {
+    for (let x = 0; x < sizeOfField; x++) {
+        let local_div = document.createElement("div");
+        local_div.classList.add("field__item");
 
-		for (let y = 0; y < sizeOfField; y++) {
-			local_div.insertAdjacentHTML(
-				"beforeend",
-				codeBlock(`${x}-${y}`, cssSizeOfBlock)
-			);
-		}
+        for (let y = 0; y < sizeOfField; y++) {
+            local_div.insertAdjacentHTML("beforeend", codeBlock(`${x}-${y}`));
+        }
 
-		field.insertAdjacentHTML("beforeend", local_div.outerHTML);
-	}
+        field.insertAdjacentHTML("beforeend", local_div.outerHTML);
+    }
 }
 
 function main() {
-	let field = document.getElementById(fieldName);
+    let field = document.getElementById(fieldName);
 
-	if (!field) {
-		console.error("Не найдено поле с таким [ID]: " + fieldName);
-		return;
-	}
+    if (!field) {
+        console.error("Не найдено поле с таким [ID]: " + fieldName);
+        return;
+    }
 
-	if (sizeOfField <= 0) {
-		console.error(
-			"Невозможно создать поле с таким [sizeOfField]: " + sizeOfField
-		);
-		return;
-	}
+    if (sizeOfField <= 0) {
+        console.error(
+            "Невозможно создать поле с таким [sizeOfField]: " + sizeOfField
+        );
+        return;
+    }
 
-	let cssSizeOfBlock = Math.round(100 / sizeOfField);
+    createField();
 
-	createField(cssSizeOfBlock);
-
-	const tictactoe = new TicTacToe(sizeOfField);
+    const tictactoe = new TicTacToe(sizeOfField);
 }
 
 main();
