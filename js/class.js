cc9fad950a3fb242e890f3b4159cb1eac11c6cf4
Daniil Dybka <danil.dybko@gmail.com>
2024-06-29 19:32:02 +0300

    Работает над логикой победы

--- js/class.js
+++ js/class.js
@@ -12,40 +12,134 @@ class Field {
         }
     }
 
+    set(x, y, status) {
+        if (this.field[y][x] === -1) {
+            this.field[y][x] = status
+        }
+    }
+
+    print() {
+        console.log(this.field)
+    }
+
+    get fieldBusy() {
+        for (let x = 0; x < this.size; x++) {
+            for (let y = 0; y < this.size; y++) {
+                if(this.field[x][y] === -1) return false
+            }
+        }
+        return true
+    }
+
     get winRow() {
+        for (let x = 0; x < this.size; x++) {
+            let [flag, firstElement] = [true, -1]
+            for (let y = 0; y < this.size; y++) {
+                if (this.field[y][x] === -1) {
+                    flag = false
+                    break
+                }
+                firstElement = firstElement === -1 ? this.field[y][x] : firstElement
+                if (this.field[y][x] !== firstElement) {
+                    flag = false
+                    break
+                }
+            }
+            if(flag) return firstElement
+        }
         return false
     }
 
     get winCol() {
+        for (let x = 0; x < this.size; x++) {
+            let [flag, firstElement] = [true, -1]
+            for (let y = 0; y < this.size; y++) {
+                if (this.field[x][y] === -1) {
+                    flag = false
+                    break
+                }
+                firstElement = firstElement === -1 ? this.field[x][y] : firstElement
+                if (this.field[x][y] !== firstElement) {
+                    flag = false
+                    break
+                }
+            }
+            if(flag) return firstElement
+        }
         return false
     }
 
     get winDiag1() {
-        return false
+        $first = 0;
+        for ($i = 0; $i < $this->len; $i++) {
+            if ($this->matrix[$i][$i] == 0) {
+                return false;
+            }
+
+            $first = $first == 0 ? $this->matrix[$i][$i] : $first;
+
+            if ($this->matrix[$i][$i] != $first) {
+                return false;
+            }
+        }
+
+        return [
+            "status" => true,
+            "win" => $first
+        ];
     }
 
     get winDiag2() {
-        return false
+        $first = 0;
+        $i = 0;
+        for ($j = $this->len - 1; $j >= 0; $j--) {
+            if ($this->matrix[$i][$j] == 0) {
+                return false;
+            }
+
+            $first = $first == 0 ? $this->matrix[$i][$j] : $first;
+
+            if ($this->matrix[$i][$j] != $first) {
+                return false;
+            }
+
+            $i += 1;
+        }
+
+        return [
+            "status" => true,
+            "win" => $first
+        ];
     }
 }
 
 class TicTacToe {
     constructor(size = 3) {
         this.field = new Field(size)
+        this.move = Math.floor(Math.random() * 2);
 	}
+
+    set(x, y) {
+        this.field.set(x, y, this.move)
+        this.move = this.move == 0 ? 1 : 0
+    }
+
+    get busy() {
+        return this.field.fieldBusy
+    }
 
     get win() {
         const winRow = this.field.winRow
-        if (winRow !== -1) return winRow
+        if (winRow !== false) return winRow
 
         const winCol = this.field.winCol
-        if(winCol !== -1) return winCol
+        if(winCol !== false) return winCol
 
         const winDiag1 = this.field.winDiag1
-        if(winDiag1 !== -1) return winDiag1
+        if(winDiag1 !== false) return winDiag1
 
         const winDiag2 = this.field.winDiag2
-        if (winDiag2 !== -1) return winDiag2
+        if (winDiag2 !== false) return winDiag2
 
         return false
     }
--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,38 @@
 const updateTitle = () => document.querySelector('h1').textContent = 'Крестики Нолики на JavaScript!'
+const updateCurrentMove = () => document.querySelector('p').innerHTML = `Текущий ход: <span>${Game.move ? 'X' : 'O'}</span>`
+const updateWin = () => {
+    const win = Game.win
+    if (win !== false) {
+        console.log('ПОБЕДА', win)
+    }
+    else {
+        const busy = Game.busy
+        if (busy) {
+            console.log('НИЧЬЯ')
+        }
+    }
+}
 
 const button = (buttonId) => {
     const button = document.getElementById(`${buttonId}`)
-    console.log(button)
+    if (button.hasAttribute('disabled')) return
+
+    buttonSetActive(button.id, Game.move ? 'X' : 'O')
+    Game.set(...button.id.split('-').map(num => parseInt(num)))
+
+    // console.log(Game.win)
+    updateUI()
 }
 
 const buttonHTML = (id) => `<button id='${id}' onclick='button("${id}")'></button>`
+
+const buttonSetActive = (id, status) => {
+    const button = document.getElementById(id)
+    button.removeAttribute('onclick')
+    button.textContent = status
+    button.setAttribute('disabled', '')
+    button.classList.add(`button-${status.toLowerCase()}`)
+}
 
 const initField = () => {
     const fieldName = 'field'
@@ -25,4 +52,6 @@ const initField = () => {
 
 const updateUI = () => {
     updateTitle()
+    updateCurrentMove()
+    updateWin()
 }
